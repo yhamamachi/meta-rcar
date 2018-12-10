10331711fd9e67bf88e475dd88540001ce54f914
Konstantin Ripak <konstantin.ripak@cogentembedded.com>
2018-12-10 16:48:41 +0300

    [recipes-core] Fix fan enable, fix USB hub initialization

--- meta-rcar-gen3-addons/recipes-core/vb2-hw-init/vb2-hw-init/vb2-hw-init.sh
+++ meta-rcar-gen3-addons/recipes-core/vb2-hw-init/vb2-hw-init/vb2-hw-init.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
 do_start() {
+    echo 1 > /sys/class/hwmon/hwmon0/pwm1_enable
+    echo 1 > /sys/class/hwmon/hwmon1/pwm1_enable
     echo 224 > /sys/class/hwmon/hwmon0/pwm1
     echo 224 > /sys/class/hwmon/hwmon1/pwm1
-    /etc/init.d/vb2-enable-fan.sh hwmon0
-    /etc/init.d/vb2-enable-fan.sh hwmon1
+    /usr/sbin/i2cset -y 12 0x44 0xf8 0x01
+    /usr/sbin/i2cset -y 12 0x45 0xf8 0x01
 }
 
 do_stop() {
 # todo: what should we do on shutdown?
+    /usr/sbin/i2cset -y 12 0x44 0xf8 0x00
+    /usr/sbin/i2cset -y 12 0x45 0xf8 0x00
     echo 0 > /sys/class/hwmon/hwmon0/pwm1
     echo 0 > /sys/class/hwmon/hwmon1/pwm1
 }
