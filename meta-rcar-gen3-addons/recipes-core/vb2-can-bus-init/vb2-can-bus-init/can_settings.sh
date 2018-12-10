a47c8b1d8f4d327c5f608d49f216f19ffe927c2b
Konstantin Ripak <konstantin.ripak@cogentembedded.com>
2018-12-10 16:47:56 +0300

    [recipes-core] Update can bus setup

--- meta-rcar-gen3-addons/recipes-core/vb2-can-bus-init/vb2-can-bus-init/can_settings.sh
+++ meta-rcar-gen3-addons/recipes-core/vb2-can-bus-init/vb2-can-bus-init/can_settings.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 /sbin/ip link set dummy0 up
-/sbin/ip link set can0 up type can bitrate 500000
-/sbin/ip link set can1 up type can bitrate 500000
+/sbin/ip link set can0 up type can bitrate 500000 sample-point 0.75 dbitrate 4000000 dsample-point 0.8 fd on
+/sbin/ip link set can1 up type can bitrate 500000 sample-point 0.75 dbitrate 4000000 dsample-point 0.8 fd on
 /sbin/ip link set can2 up type can bitrate 500000
 /sbin/ip link set can3 up type can bitrate 500000
 
